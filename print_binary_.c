6edb6e393886cd0f5ef6a7730f240b41a6708bf7
Oiyanu <iyanuolatomide@gmail.com>
2023-10-14 11:21:33 -0700

    Education is when you read the fine print. Experience is what you get if you don't

---
+++ print_binary_.c
@@ -0,0 +1,44 @@
+/**
+ * print_binary_ - Function prints an unsigned number
+ * @forms: List of arguments
+ * @buffer: Buffer array to handle print
+ * @flags: To calc active flags.
+ * @width: get width
+ * @precision: Specific precision
+ * @size: Size specifier
+ * Return: The number of characters to be printed.
+ */
+int print_binary(va_list forms, char buffer[],
+	int flags, int width, int precision, int size)
+{
+	unsigned int n, g, y, sum;
+	unsigned int b[32];
+	int tally;
+
+	UNUSED(buffer);
+	UNUSED(flags);
+	UNUSED(width);
+	UNUSED(precision);
+	UNUSED(size);
+
+	n = va_arg(forms, unsigned int);
+	g = 2147483648;
+	b[0] = n / g;
+	for (y = 1; y < 32; y++)
+	{
+		g /= 2;
+		b[y] = (n / g) % 2;
+	}
+	for (y = 0, sum = 0, tally = 0; y < 32; y++)
+	{
+		sum += b[y];
+		if (sum || y == 31)
+		{
+			char s = '0' + b[y];
+
+			write(1, &s, 1);
+			tally++;
+		}
+	}
+	return (tally);
+}
--- print_int_.c
+++ print_int_.c
@@ -1,7 +1,7 @@
 /**
- * print_int_ - print integer
- * @types: the list of parameters
- * @buffers: Buffer array to handle print
+ * print_int - print integer
+ * @forms: the list of parameters
+ * @buffer: Buffer array to handle print
  * @flags: to calculate the active flags
  * @width: get width
  * @size: to specify the size
@@ -20,20 +20,17 @@ int print_int(va_list forms, char buffer[], int flags, int width, int precision,
 
 	if (n == 0)
 		buffer[k--] = '0';
-
 	buffer[BUFF_SIZE - 1] = '\0';
 	nos = (unsigned long int)n;
-
-	if(n < 0)
+	if (n < 0)
 	{
-		nos = (unsigned long int)((-1)*n);
+		nos = (unsigned long int)((-1) * n);
 		neg = 1;
 	}
-
 	while (nos > 0)
 	{
 		buffer[k--] = (nos % 10) + '0';
-		nos/= 10;
+		nos /= 10;
 	}
 
 	k++;
