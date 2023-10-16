6c09f6c5fc21048591a022f26f3a220361dc57b3
justine1285 <justineadugbo@gmail.com>
2023-10-16 03:06:11 -0700

    Education is when you read the fine print. Experience is what you get if you don't

---
+++ print_int.c
@@ -0,0 +1,44 @@
+#include "main.h"
+/**
+ * printf_int - prints integers
+ * @arg: prints argument
+ * Return: number of characters printed
+ */
+int print_int(va_list arg)
+{
+	int x = va_arg(args, int);
+	int y, last = n % 10, digit, exp = 1;
+	int z = 1;
+
+	x = x / 10;
+	y = x;
+
+	if (last < 0)
+	{
+		_putchar('-');
+		y = -y;
+		x = -x;
+		last = -last;
+		z++;
+	}
+	if (y > 0)
+	{
+		while (y / 10 != 0)
+		{
+			exp = exp * 10;
+			y = y / 10;
+		}
+		y = x;
+		while (exp > 0)
+		{
+			digit = y / exp;
+			_putchar(digit + '0');
+			y = y - (digit * exp);
+			exp = exp / 10;
+			z++;
+		}
+	}
+	_putchar(last + '0');
+
+	return (z);
+}
