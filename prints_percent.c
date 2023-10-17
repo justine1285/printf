6fa34a38153ecaf2abcdab0e0c9011044dd6b78e
justine1285 <justineadugbo@gmail.com>
2023-10-17 09:37:24 -0700

    update

--- prints_percent.c
+++ prints_percent.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
  * prints_percent - prints percentage
+ * @forms: argument
  * Return: the amount of characters printed
  */
-
-int prints_percent(__attribute__((unused))va_list list)
+int prints_percent(__attribute__((unused))va_list forms)
 {
 	_putchars('%');
 	return (1);
